aeeb90c2a694d60075125a1b6466f512553fa8e5
2252819957 <wei.wutao@rongyu100.com>
2016-01-19 16:58:09 +0800

    code fix

--- RYTableFormList/RYTableFormList/ViewController.m
+++ RYTableFormList/RYTableFormList/ViewController.m
@@ -135,6 +135,52 @@
     [groupLayer addAnimation:groupAnnimation forKey:@"groupAnnimation"];
 }
 
+/*
+//渐变色
+
+//Transparent Gradient Layer
+- (void) insertTransparentGradient {
+    
+    UIColor *colorOne = [UIColor colorWithRed:(33/255.0)  green:(33/255.0)  blue:(33/255.0)  alpha:0.0];
+    UIColor *colorTwo = [UIColor colorWithRed:(33/255.0)  green:(33/255.0)  blue:(33/255.0)  alpha:1.0];
+    NSArray *colors = [NSArray arrayWithObjects:(id)colorOne.CGColor, colorTwo.CGColor, nil];
+    NSNumber *stopOne = [NSNumber numberWithFloat:0.0];
+    NSNumber *stopTwo = [NSNumber numberWithFloat:1.0];
+    NSArray *locations = [NSArray arrayWithObjects:stopOne, stopTwo, nil];
+    
+    //crate gradient layer
+    CAGradientLayer *headerLayer = [CAGradientLayer layer];
+    
+    headerLayer.colors = colors;
+    headerLayer.locations = locations;
+    headerLayer.frame = self.bounds;
+    
+    [self.layer insertSublayer:headerLayer atIndex:0];
+}
+
+
+//color gradient layer
+- (void) insertColorGradient {
+    
+    UIColor *colorOne = [UIColor colorWithRed:(255/255.0) green:(255/255.0) blue:(255/255.0) alpha:1.0];
+    UIColor *colorTwo = [UIColor colorWithRed:(33/255.0)  green:(33/255.0)  blue:(33/255.0)  alpha:1.0];
+    
+    NSArray *colors = [NSArray arrayWithObjects:(id)colorOne.CGColor, colorTwo.CGColor, nil];
+    NSNumber *stopOne = [NSNumber numberWithFloat:0.0];
+    NSNumber *stopTwo = [NSNumber numberWithFloat:1.0];
+    
+    NSArray *locations = [NSArray arrayWithObjects:stopOne, stopTwo, nil];
+    CAGradientLayer *headerLayer = [CAGradientLayer layer];
+    headerLayer.colors = colors;
+    headerLayer.locations = locations;
+    headerLayer.frame = self.bounds;
+    
+    [self.layer insertSublayer:headerLayer above:0];
+    
+}
+ 
+ */
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
