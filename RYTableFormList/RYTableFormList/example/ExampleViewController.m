3f1bd28928e8998125041606a9c0b2819b0d5f7e
juanfeng <zhangzhujuan@ZHANGiMac.local>
2016-02-02 16:10:02 +0800

    与标题换行输入多行文字定义
    与标题不在一行的多行输入框如备注或评价之类TextView的cell定义

--- RYTableFormList/RYTableFormList/example/ExampleViewController.m
+++ RYTableFormList/RYTableFormList/example/ExampleViewController.m
@@ -27,6 +27,11 @@
     [super viewDidLoad];
     
     [self.view addSubview:self.contentTableView];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTapGesture:)];
+    tapGesture.numberOfTapsRequired = 1;
+    tapGesture.numberOfTouchesRequired = 1;
+    [self.contentTableView addGestureRecognizer:tapGesture];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,101 +41,139 @@
 #pragma mark - UITableViewDelegate & UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return 6;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (4 == indexPath.row || 5 == indexPath.row) {
+        
+        return 150.0f;
+    }
     return 45;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    TraditionPickersTableViewCell *traditionPickersTableViewCell = [tableView dequeueReusableCellWithIdentifier:@"TraditionDatePickerElement"];
-    
-    if (!traditionPickersTableViewCell) {
-        traditionPickersTableViewCell = [[TraditionPickersTableViewCell alloc] init];
+    if (4 == indexPath.row || 5 == indexPath.row) {
+        
+        TraditionCustomerTableViewCell *traditionCustomerTableViewCell = [TraditionCustomerTableViewCell initCellWithTableView:tableView];
+        
+        if (4 == indexPath.row) {
+            
+            traditionCustomerTableViewCell.titleLabel.text = @"备注";
+            
+        }else{
+            
+            traditionCustomerTableViewCell.titleLabel.text = @"评价如下：";
+            //可设置颜色
+            traditionCustomerTableViewCell.titleView.backgroundColor = [UIColor whiteColor];
+            //可设置显示不显示
+            traditionCustomerTableViewCell.redImgBtn.backgroundColor = [UIColor redColor];
+            //可设置显示不显示
+            traditionCustomerTableViewCell.tipBtn.backgroundColor = [UIColor purpleColor];
+            [traditionCustomerTableViewCell.tipBtn addTarget:self action:@selector(TipRemarkAction) forControlEvents:UIControlEventTouchUpInside];
+        }
+        
+        traditionCustomerTableViewCell.inputTV.text = @"请填写";
+        
+        traditionCustomerTableViewCell.selectionStyle = UITableViewCellSelectionStyleNone;
+        return traditionCustomerTableViewCell;
+        
+    }else{
+        
+        TraditionPickersTableViewCell *traditionPickersTableViewCell = [tableView dequeueReusableCellWithIdentifier:@"TraditionDatePickerElement"];
+        
+        if (!traditionPickersTableViewCell) {
+            traditionPickersTableViewCell = [[TraditionPickersTableViewCell alloc] init];
+        }
+        
+        NSString *content     = @"";
+        NSString *unitContent = @"";
+        NSString *itemContent = @"";
+        UITableViewCellUnitType unitType = UITableViewCellUnitTypeHave;
+        
+        if (0 == indexPath.row) {
+            
+            //unit show
+            content     = @"单位";
+            unitContent = @"元";
+            itemContent = @"ModeDateAndTime";
+            unitType    = UITableViewCellUnitTypeHave;
+            
+        }else if (1 == indexPath.row){
+            
+            //unit hidden
+            content     = @"无单位";
+            itemContent = @"ModeDateOnly";
+            unitType    = UITableViewCellUnitTypeNone;
+            
+        }else {
+            
+            content     = @"单位";
+            unitContent = @"元";
+            itemContent = @"ModeDateAndTime";
+            unitType    = UITableViewCellUnitTypeHave;
+            
+        }
+        
+        traditionPickersTableViewCell.content     = content;
+        traditionPickersTableViewCell.unitContent = unitContent;
+        traditionPickersTableViewCell.itemContent = itemContent;
+        traditionPickersTableViewCell.unitType    = unitType;
+        [traditionPickersTableViewCell reloadData];
+        
+        return traditionPickersTableViewCell;
     }
-    
-    NSString *content     = @"";
-    NSString *unitContent = @"";
-    NSString *itemContent = @"";
-    UITableViewCellUnitType unitType = UITableViewCellUnitTypeHave;
-    
-    if (0 == indexPath.row) {
-        
-        //unit show
-        content     = @"单位";
-        unitContent = @"元";
-        itemContent = @"ModeDateAndTime";
-        unitType    = UITableViewCellUnitTypeHave;
-        
-    }else if (1 == indexPath.row){
-        
-        //unit hidden
-        content     = @"无单位";
-        itemContent = @"ModeDateOnly";
-        unitType    = UITableViewCellUnitTypeNone;
-        
-    }else {
-        
-        content     = @"单位";
-        unitContent = @"元";
-        itemContent = @"ModeDateAndTime";
-        unitType    = UITableViewCellUnitTypeHave;
-        
-    }
-    
-    traditionPickersTableViewCell.content     = content;
-    traditionPickersTableViewCell.unitContent = unitContent;
-    traditionPickersTableViewCell.itemContent = itemContent;
-    traditionPickersTableViewCell.unitType    = unitType;
-    [traditionPickersTableViewCell reloadData];
-    
-    return traditionPickersTableViewCell;
     
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    PickerView *pickerView = [[UIFactory shareInstance] pickerView];
-    pickerView.pickerViewDelegte = self;
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    if (0 == indexPath.row) {
+    if (4 == indexPath.row || 5 == indexPath.row) {
+        return;
+    }else{
         
-        //TODO:测试
-        pickerView.popUpPickerViewType  = PickerViewTypeDate;
-        //如果是日期控件，需要设置控件日期类型
-        pickerView.datePickerViewMode   = PickerViewDateModeDateAndTime;
+        PickerView *pickerView = [[UIFactory shareInstance] pickerView];
+        pickerView.pickerViewDelegte = self;
         
-    }else if (1 == indexPath.row){
+        if (0 == indexPath.row) {
+            
+            //TODO:测试
+            pickerView.popUpPickerViewType  = PickerViewTypeDate;
+            //如果是日期控件，需要设置控件日期类型
+            pickerView.datePickerViewMode   = PickerViewDateModeDateAndTime;
+            
+        }else if (1 == indexPath.row){
+            
+            //TODO:测试
+            pickerView.popUpPickerViewType  = PickerViewTypeDate;
+            //如果是日期控件，需要设置控件日期类型
+            pickerView.datePickerViewMode   = PickerViewDateModeDateOnly;
+            
+        }else if (2 == indexPath.row) {
+            
+            pickerView.popUpPickerViewType  = PickerViewTypeNormal;
+            pickerView.normalPickerViewType = PickerViewNormalTypeOne;
+            
+            pickerView.dataSource = [NSMutableArray arrayWithObjects:@[@"有",@"无"], nil];
+            
+        }else if (3 == indexPath.row) {
+            
+            pickerView.popUpPickerViewType  = PickerViewTypeNormal;
+            pickerView.normalPickerViewType = PickerViewNormalTypeTwo;
+            
+            pickerView.dataSource = [NSMutableArray arrayWithObjects:@[@"row1",@"row2"],@[@"row3",@"row4"], nil];
+        }
         
-        //TODO:测试
-        pickerView.popUpPickerViewType  = PickerViewTypeDate;
-        //如果是日期控件，需要设置控件日期类型
-        pickerView.datePickerViewMode   = PickerViewDateModeDateOnly;
+        [self.view addSubview:pickerView];
         
-    }else if (2 == indexPath.row) {
-        
-        pickerView.popUpPickerViewType  = PickerViewTypeNormal;
-        pickerView.normalPickerViewType = PickerViewNormalTypeOne;
-        
-        pickerView.dataSource = [NSMutableArray arrayWithObjects:@[@"有",@"无"], nil];
-        
-    }else if (3 == indexPath.row) {
-        
-        pickerView.popUpPickerViewType  = PickerViewTypeNormal;
-        pickerView.normalPickerViewType = PickerViewNormalTypeTwo;
-        
-        pickerView.dataSource = [NSMutableArray arrayWithObjects:@[@"row1",@"row2"],@[@"row3",@"row4"], nil];
+        [pickerView animationStart];
+        //    pickerView.dataSource = [NSMutableArray arrayWithObjects:@[@"1",@"2",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3"],@[@"1",@"2",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3"],@[@"1",@"2",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3"], nil];
+        [pickerView reloadData];
     }
-
-    [self.view addSubview:pickerView];
-    
-    [pickerView animationStart];
-//    pickerView.dataSource = [NSMutableArray arrayWithObjects:@[@"1",@"2",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3"],@[@"1",@"2",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3"],@[@"1",@"2",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3",@"3"], nil];
-    [pickerView reloadData];
-    
-    
 }
 
 #pragma mark - PickerViewDelegte
@@ -146,13 +189,26 @@
 }
 
 #pragma mark - event response
+
+- (void)onTapGesture:(UITapGestureRecognizer*)tap{
+    
+    if (![tap.view isKindOfClass:[UITextView class]] || ![tap.view isKindOfClass:[UITextField class]]) {
+        [self.view endEditing:YES];
+    }
+}
+
+- (void)TipRemarkAction{
+    
+    NSLog(@"或弹出提示信息");
+}
+
 #pragma mark - private method
 #pragma mark - getters and setters
 
 - (UITableView *)contentTableView {
     
     if (!_contentTableView) {
-        _contentTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, SCREEN_BOUND_WIDTH, SCREEN_BOUND_HEIGHT) style:UITableViewStylePlain];
+        _contentTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, SCREEN_BOUND_WIDTH, SCREEN_BOUND_HEIGHT) style:UITableViewStyleGrouped];
         _contentTableView.delegate = self;
         _contentTableView.dataSource = self;
     }
--- RYTableFormList/RYTableFormList/viewControllers/TraditionCustomerTableViewCell.h
+++ RYTableFormList/RYTableFormList/viewControllers/TraditionCustomerTableViewCell.h
@@ -9,7 +9,28 @@
 #import <UIKit/UIKit.h>
 #import "RYTableViewCell.h"
 
+typedef NS_ENUM(NSUInteger, TableViewCellStyle) {
+   TableViewCellStyleRemarks,//填写备注信息之类的
+};
+
 //其他自定义视图
-@interface TraditionCustomerTableViewCell : RYTableViewCell
+@interface TraditionCustomerTableViewCell : RYTableViewCell<UITextViewDelegate>
+
+/*
+ 属性/控件
+ */
+@property (nonatomic, assign) TableViewCellStyle tableViewCellStyle;
+
+@property (nonatomic, strong) UIView       *titleView;
+@property (nonatomic, strong) UIButton     *redImgBtn;
+@property (nonatomic, strong) UILabel      *titleLabel;
+@property (nonatomic, strong) UIView       *labelBotLine;
+@property (nonatomic, strong) UIButton     *tipBtn;
+@property (nonatomic, strong) UITextView   *inputTV;
+
+/*
+ 方法
+ */
++ (instancetype)initCellWithTableView:(UITableView*)tableView;
 
 @end
